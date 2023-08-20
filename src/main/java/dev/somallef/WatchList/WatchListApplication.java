65fe49f335fc2a7f17bfe083c4cf9e9087b89c1b
Allef Mizael <allef.mizael@gmail.com>
2023-08-20 18:53:07 -0300

    Habilitando o CommandLineRunner e criando a classe para consumo da API do OMDB

--- src/main/java/dev/somallef/WatchList/WatchListApplication.java
+++ src/main/java/dev/somallef/WatchList/WatchListApplication.java
@@ -1,13 +1,24 @@
 package dev.somallef.WatchList;
 
+import dev.somallef.WatchList.service.APIConsumer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WatchListApplication {
+public class WatchListApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WatchListApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		var url = "https://www.omdbapi.com/?t=The+Sopranos&apikey=d8d4d5a9";
+		var apiConsumer = new APIConsumer();
+		var json = apiConsumer.getData(url);
+		System.out.println(json);
+
+	}
 }
