853f2ca7e89a26e326def51ce7a2a9ad45ac749c
Allef Mizael <allef.mizael@gmail.com>
2023-08-20 18:11:51 -0300

    Criação do projeto

---
+++ src/test/java/dev/somallef/WatchList/WatchListApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.somallef.WatchList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
