52d90b8cacc5a6e95457ac3daf8f392bcc4a34be
Allef Mizael <allef.mizael@gmail.com>
2024-05-17 21:16:21 -0300

    Commit inicial

---
+++ src/test/java/com/allefmizael/watchlistapi/WatchlistApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allefmizael.watchlistapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchlistApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
